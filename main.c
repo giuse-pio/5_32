3eaef1474ba708ac737d7fb8ae22737e8f5af5df
giuse-pio <giupeppepio7@gmail.com>
2025-02-04 17:53:18 +0100

    aggiunta del bubble sort con sentinella

--- main.c
+++ main.c
@@ -3,8 +3,15 @@
 #include <time.h>
 
 int main(void) {
-
-    float voti[20];
+    int n;
+    printf("inserisci il numero di voti:");
+    do {
+        scanf("%d", &n);
+        if (n < 1 || n > 20) {
+            printf("Il numero di voti deve essere compreso tra 1-20\n");
+        }
+    } while (n < 1 || n > 20);
+    float voti[n];
 
     float VotomMax = 0, MediaVoti = 0, SommaVoti = 0;
 
@@ -12,9 +19,9 @@ int main(void) {
 
     srand(time(NULL));
 
-    printf("inserisci 20 voti :");
+    printf("inserisci %d voti :", n);
 
-    for (int i = 0; i < 20; i++) {
+    for (int i = 0; i < n; i++) {
         do {
             scanf("%f", &voti[i]);
         } while (voti[i] < 2 || voti[i] > 10);
@@ -28,32 +35,47 @@ int main(void) {
         }
 
         SommaVoti += voti[i];
-        MediaVoti = SommaVoti / 20;
+        MediaVoti = SommaVoti / n;
 
         if (voti[i] == 6) {
             crediti = (rand() % 2) + 7; // Genera casualmente i crediti tra 7 e 8
-        }
-        else if (voti[i] > 6 && voti[i] <= 7) {
+        } else if (voti[i] > 6 && voti[i] <= 7) {
             crediti = (rand() % 2) + 8; // Genera casualmente i crediti tra 8 e 9
-        }
-        else if (voti[i] > 7 && voti[i] <= 8) {
+        } else if (voti[i] > 7 && voti[i] <= 8) {
             crediti = (rand() % 2) + 9; // Genera casualmente i crediti tra 9 e 10
-        }
-        else if (voti[i] > 8 && voti[i] <= 9) {
+        } else if (voti[i] > 8 && voti[i] <= 9) {
             crediti = (rand() % 2) + 10; // Genera casualmente i crediti tra 10 e 11
-        }
-        else if (voti[i] > 9 && voti[i] <= 10) {
+        } else if (voti[i] > 9 && voti[i] <= 10) {
             crediti = (rand() % 2) + 11; // Genera casualmente i crediti tra 11 e 12
-        }
-        else {
+        } else {
             crediti = 0;
         }
 
         SommaCrediti += crediti;
-
     }
     printf("voto max = %.2f\n", VotomMax);
     printf("le materie con il voto piu' alto sono %d\n", NumMatVotoMax);
-    printf("la media di 20 voti e' %.2f\n", MediaVoti);
+    printf("la media di %d voti e' %.2f\n", n, MediaVoti);
     printf("i crediti totale sono %d\n", SommaCrediti);
+
+
+    for (int i = 1; i < n + 1; i++) {
+        int Scambi = 0;
+
+        for (int j = 0; j < n - i; j++) {
+            if (voti[j] > voti[j + 1]) {
+                int temp = 0;
+                temp = voti[j];
+                voti[j] = voti[j + 1];
+                voti[j + 1] = temp;
+                Scambi = 1;
+            }
+        }
+        if (Scambi == 0)
+            break;
+    }
+
+    for (int i = 0; i < n; i++) {
+        printf("%.2f ", voti[i]);
+    }
 }
